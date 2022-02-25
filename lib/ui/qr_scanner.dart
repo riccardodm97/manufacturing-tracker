849527a53b0e49a4a840158127e31c2a66d48a56
JackBerselli <35583166+JackBerselli@users.noreply.github.com>
2022-02-25 10:00:46 +0100

    new minsdkversion to 21

--- lib/ui/qr_scanner.dart
+++ lib/ui/qr_scanner.dart
@@ -4,28 +4,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-void main() => runApp(const MaterialApp(home: MyHome()));
-
-class MyHome extends StatelessWidget {
-  const MyHome({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => const QRViewScanner(),
-            ));
-          },
-          child: const Text('qrView'),
-        ),
-      ),
-    );
-  }
-}
-
 class QRViewScanner extends StatefulWidget {
   const QRViewScanner({Key? key}) : super(key: key);
 
