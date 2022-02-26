c152b183f517eac09d9df48b429bd109de13731c
JackBerselli <35583166+JackBerselli@users.noreply.github.com>
2022-02-26 13:42:16 +0100

    blockchain + firebase create product

--- lib/ui/create_product_view.dart
+++ lib/ui/create_product_view.dart
@@ -6,15 +6,17 @@ import '../viewmodel/create_product_view_model.dart';
 class CreateProductView extends StatelessWidget {
   CreateProductView({Key? key}) : super(key: key);
 
-  final nameController = TextEditingController();
-  final dateController = TextEditingController();
-  final idController = TextEditingController();
+  List<String> components = [];
+
+  final productNameController = TextEditingController();
+  final manufacturerNameController = TextEditingController();
+  final productionLocationController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<CreateProductViewModel>.reactive(
         viewModelBuilder: () => CreateProductViewModel(),
-        builder: (context, viewModel, child) => Scaffold(
+        builder: (context, model, child) => Scaffold(
             resizeToAvoidBottomInset: false,
             appBar: AppBar(
               title: const Text('Create Product'),
@@ -30,7 +32,7 @@ class CreateProductView extends StatelessWidget {
                         padding:
                             const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 12.0),
                         child: TextField(
-                            controller: nameController,
+                            controller: productNameController,
                             decoration: InputDecoration(
                               enabledBorder: const OutlineInputBorder(
                                   borderSide: BorderSide(
@@ -47,7 +49,7 @@ class CreateProductView extends StatelessWidget {
                         padding:
                             const EdgeInsets.fromLTRB(24.0, 12.0, 24.0, 12.0),
                         child: TextField(
-                            controller: dateController,
+                            controller: manufacturerNameController,
                             decoration: InputDecoration(
                               enabledBorder: const OutlineInputBorder(
                                   borderSide: BorderSide(
@@ -64,7 +66,7 @@ class CreateProductView extends StatelessWidget {
                         padding:
                             const EdgeInsets.fromLTRB(24.0, 12.0, 24.0, 12.0),
                         child: TextField(
-                            controller: idController,
+                            controller: productionLocationController,
                             decoration: InputDecoration(
                               enabledBorder: const OutlineInputBorder(
                                   borderSide: BorderSide(
@@ -81,35 +83,95 @@ class CreateProductView extends StatelessWidget {
                         padding: const EdgeInsets.all(24.0),
                         child: FloatingActionButton.extended(
                             heroTag: "add_component_button",
-                            onPressed: () async {},
-                            // async {
-                            //   components = await Navigator.pushNamed(
-                            //       context, '/component');
-                            //   setState(() {
-                            //     listComponents(components);
-                            //   });
-                            //
+                            onPressed: () async {
+                              await model.getPossibleComponents();
+                              components = await showDialog(
+                                context: context,
+                                builder: (BuildContext context) =>
+                                    _buildPopupDialog(context, model),
+                              );
+                            },
                             backgroundColor: Colors.blue[400],
-                            icon: Icon(Icons.add),
-                            label: Text('Add component')),
+                            icon: const Icon(Icons.add),
+                            label: const Text('Add component')),
                       ),
+                      Flexible(child: listComponents(components)),
                       Padding(
                         padding: const EdgeInsets.all(24.0),
                         child: FloatingActionButton.extended(
                             heroTag: "save_button",
-                            onPressed: () {},
-                            //   viewModel.addProduct(Product(
-                            //       name: nameController.text,
-                            //       date: dateController.text,
-                            //       id: idController.text,
-                            //       components:
-                            //           viewModel.getComponentsName(components)));
-                            //   // print(viewModel.getProducts());
-                            // },
+                            onPressed: () {
+                              model.saveNewProduct(
+                                  productNameController.text,
+                                  manufacturerNameController.text,
+                                  productionLocationController.text);
+                            },
                             backgroundColor: Colors.red[400],
-                            icon: Icon(Icons.save),
-                            label: Text('Save')),
+                            icon: const Icon(Icons.save),
+                            label: const Text('Save')),
                       ),
                     ])))));
   }
+}
+
+Widget _buildPopupDialog(BuildContext context, CreateProductViewModel model) {
+  return AlertDialog(
+    title: const Text('My Products'),
+    content: SizedBox(
+      width: 200.0,
+      height: 300.0,
+      child: ListView.builder(
+          itemCount: model.allUserComponents.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
+              child: Card(
+                child: ListTile(
+                    onTap: () {
+                      model.addToComponentsList(model.allUserComponents[index]);
+                    },
+                    title: Text(model.allUserComponents[index],
+                        style: const TextStyle(fontSize: 24.0)),
+                    leading: const CircleAvatar(
+                        child: Icon(Icons.bookmark_border_outlined))),
+                color: Colors.grey[200],
+              ),
+            );
+          }),
+    ),
+    actions: <Widget>[
+      FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context, model.selectedComponents);
+        },
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.save_alt),
+      ),
+    ],
+  );
+}
+
+Widget listComponents(components) {
+  return ListView.builder(
+    itemCount: components?.length ?? 0,
+    itemBuilder: (context, index) {
+      return Row(children: <Widget>[
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.fromLTRB(24.0, 12.0, 24.0, 12.0),
+          child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 1.5),
+                  borderRadius: const BorderRadius.all(Radius.circular(4.0) //
+                      )),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('${index + 1}° Component: ${components[index]}',
+                    style: const TextStyle(color: Colors.white)),
+              )),
+        )),
+      ]);
+    },
+  );
 }
--- lib/viewmodel/create_product_view_model.dart
+++ lib/viewmodel/create_product_view_model.dart
@@ -9,34 +9,40 @@ class CreateProductViewModel extends BaseModel {
   final ProductService _productService = serviceLocator<ProductService>();
   final AuthService _authService = serviceLocator<AuthService>();
 
-  final List<String> _componentsList = [];
+  final List<String> _selectedComponents = [];
+  final List<String> _allUserComponents = [];
 
-  void openComponentsPopup(BuildContext context) async {
-    throw UnimplementedError();
-  }
+  List<String> get selectedComponents => _selectedComponents;
+  List<String> get allUserComponents => _allUserComponents;
 
   void addToComponentsList(String productAddress) {
-    _componentsList.add(productAddress);
+    _selectedComponents.add(productAddress);
   }
 
-  Future<List<String>> getPossibleComponents() async {
-    return await _productService
+  Future<String> getProductName(String productAddress) async {
+    await _productService.setCurrentProduct(productAddress);
+    return await _productService.getName();
+  }
+
+  Future<void> getPossibleComponents() async {
+    List<String> productList = await _productService
         .getUserProducts(_authService.userAddress.toString());
+    allUserComponents.addAll(productList);
   }
 
   void saveNewProduct(String name, String manName, String location) async {
     _productService.clearCurrentProduct();
 
     //create the product with given fields
-    var hash = await _productService.createProduct(
-        name, manName, location, BigInt.parse(DateTime.now().toString()));
+    var hash = await _productService.createProduct(name, manName, location,
+        BigInt.parse('123')); //DateTime.now().toString()));
 
     String addr = await _productService.getNewProductAddress(hash);
 
     await _productService.addProductToUser(
         _authService.userAddress.toString(), addr);
 
-    for (String c in _componentsList) {
+    for (String c in _selectedComponents) {
       await _productService.setCurrentProduct(c);
       await _productService.markAsUsed();
       _productService.clearCurrentProduct();
@@ -44,7 +50,7 @@ class CreateProductViewModel extends BaseModel {
 
     await _productService.setCurrentProduct(addr);
 
-    for (String c in _componentsList) {
+    for (String c in _selectedComponents) {
       await _productService.addConstituent(c);
     }
 
