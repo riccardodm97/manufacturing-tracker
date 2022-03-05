0017ecae5f7462ca1519c41d41f4ccf90ce5e85b
Riccardo De Matteo <51266633+riccardodm97@users.noreply.github.com>
2022-03-05 19:13:00 +0100

    test

--- lib/setup/config.dart
+++ lib/setup/config.dart
@@ -1,7 +1,11 @@
 class Config {
-  static const String _rpcURL = "http://10.0.2.2:7545";
-  static const String _wsURL = "ws://10.0.2.2:7545/";
+  static const String _rpcURL = "http://192.168.1.245:7545";
+  static const String _wsURL = "ws://192.168.1.245:7545";
 
   static String get rpcURL => _rpcURL;
   static String get wsURL => _wsURL;
 }
+
+
+//http://10.0.2.2:7545";
+//"ws://10.0.2.2:7545/";
