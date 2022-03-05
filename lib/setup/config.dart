6b711018fc032c10cbdd5a88213086be185a0855
Riccardo De Matteo <51266633+riccardodm97@users.noreply.github.com>
2022-03-05 19:13:59 +0100

    quick fix

--- lib/setup/config.dart
+++ lib/setup/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String _rpcURL = "http://192.168.1.245:7545";
-  static const String _wsURL = "ws://192.168.1.245:7545";
+  static const String _wsURL = "ws://192.168.1.245:7545/";
 
   static String get rpcURL => _rpcURL;
   static String get wsURL => _wsURL;
