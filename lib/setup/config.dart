136f3473ebbc4cdae0b3b57a86e2e163c8160d48
JackBerselli <35583166+JackBerselli@users.noreply.github.com>
2022-03-06 19:29:36 +0100

    Update config.dart

--- lib/setup/config.dart
+++ lib/setup/config.dart
@@ -1,10 +1,13 @@
 class Config {
-  static const String _rpcURL = "http://192.168.1.245:7545";
-  static const String _wsURL = "ws://192.168.1.245:7545/";
+  static const String _rpcURL = "http://192.168.1.9:7545";
+  static const String _wsURL = "ws://192.168.1.9:7545/";
 
   static String get rpcURL => _rpcURL;
   static String get wsURL => _wsURL;
 }
+
+// GIACOMO: "http://192.168.1.245:7545" - "ws://192.168.1.245:7545/"
+// RICCARDO: "http://192.168.1.9:7545" - "ws://192.168.1.9:7545/"
 
 
 //http://10.0.2.2:7545";
