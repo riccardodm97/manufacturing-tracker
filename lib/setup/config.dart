605a040c1b665372ce5c3120952c1e662d1294a0
Riccardo De Matteo <51266633+riccardodm97@users.noreply.github.com>
2022-03-07 16:13:33 +0100

    fix endpoint

--- lib/setup/config.dart
+++ lib/setup/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String _rpcURL = "http://10.0.2.2:7545";
-  static const String _wsURL = "ws:/10.0.2.2:7545/";
+  static const String _wsURL = "ws://10.0.2.2:7545/";
 
   static String get rpcURL => _rpcURL;
   static String get wsURL => _wsURL;
