5a5d1de9af1a1a61a91ebb71f7a064573f8122e5
Riccardo De Matteo <51266633+riccardodm97@users.noreply.github.com>
2022-03-07 20:58:33 +0100

    comments

--- lib/setup/config.dart
+++ lib/setup/config.dart
@@ -6,7 +6,7 @@ class Config {
   static String get wsURL => _wsURL;
 }
 
-
-// LOCALHOST: "http://127.0.0.1:7545"
-// EMULATOR LOCALHOST: "http://10.0.2.2:7545"
-
+// you can select the host and port from the server menu in the ganache app and copy it here 
+// LOCALHOST: "http://127.0.0.1:7545/"
+// EMULATOR LOCALHOST: "http://10.0.2.2:7545/"
+// you can also use here your wifi url
